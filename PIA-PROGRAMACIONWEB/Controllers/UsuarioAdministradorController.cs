63b05fbda39f9b51d5bdf5f3d74f7c20e7964aa8
Axel <axel.diazcs@uanl.edu.mx>
2024-11-24 03:21:13 -0600

    Axel Eduardo Diaz Castro 2096004 Pantallas Crud 1/2

--- PIA-PROGRAMACIONWEB/Controllers/UsuarioAdministradorController.cs
+++ PIA-PROGRAMACIONWEB/Controllers/UsuarioAdministradorController.cs
@@ -8,5 +8,83 @@ namespace PIA_PROGRAMACIONWEB.Controllers
         {
             return View();
         }
-    }
+
+        public IActionResult acerca()
+        {
+            return View();
+        }
+
+        public IActionResult agregar_evento()
+        {
+            return View();
+        }
+
+        public IActionResult asis_ensayos_detalles()
+        {
+            return View();
+        }
+
+        public IActionResult asis_eventos_detalles()
+        {
+            return View();
+        }
+
+        public IActionResult asistencia_eventos()
+        {
+            return View();
+        }
+
+        public IActionResult audiciones()
+        {
+            return View();
+        }
+
+        public IActionResult carrusel()
+        {
+            return View();
+        }
+
+        public IActionResult contactanos()
+        {
+            return View();
+        }
+
+        public IActionResult crear_cuentas()
+        {
+            return View();
+        }
+
+        public IActionResult cuentas()
+        {
+            return View();
+        }
+
+        public IActionResult detalles_ensayos()
+        {
+            return View();
+        }
+
+        public IActionResult detalles_eventos()
+        {
+            return View();
+        }
+
+        public IActionResult ensayos()
+        {
+            return View();
+        }
+
+        public IActionResult estado()
+        {
+            return View();
+        }
+
+
+
+
+
+
+
+
+    } 
 }
