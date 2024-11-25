c17ac10532d77465647116f5ad4e6f8f6d4c6a39
David Meza <josemezaf@uanl.edu.mx>
2024-11-24 22:28:52 -0600

    Se corrigieron errores del home

---
+++ PIA-PROGRAMACIONWEB/Views/UsuarioNoRegistrado/Home.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PIA_PROGRAMACIONWEB.Views.UsuarioNoRegistrado
+{
+    public class HomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
